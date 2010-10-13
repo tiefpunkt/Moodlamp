ef9faf836acd42e697c1ec24e1c643b902865d4b
Severin Schols <severin@schols.de>
2010-10-14 01:35:01 +0200

    Initial Commit
     - Ported CANmood from das-labor.org to the RZL moodlamp
     - removed anything CAN-related
     - simple fading is working very well :)

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <avr/io.h>
+
+
+#include "config.h"
+#include <avr/interrupt.h>
+
+//#include "can/can.h"
+//#include "can_handler.h"
+//#include "can/spi.h"
+//#include "can/lap.h"
+
+#include "pwm.h"
+#include "static_scripts.h"
+#include "testscript.h"
+
+
+void init(void)
+{
+	ACSR = _BV(ACD); // Disable Analog Comparator (power save)
+
+	//initialize spi port
+//	spi_init();
+	
+	//initialize can communication
+	//can_init();
+	//read_can_addr();
+	//turn on interrupts
+	init_pwm();
+	sei();
+}
+
+
+int main(void)
+{
+	init();
+	
+	
+	init_script_threads();
+	script_threads[0].handler.execute = &memory_handler_eeprom;
+	script_threads[0].handler.position = (uint16_t) &colorchange_red;
+	script_threads[0].flags.disabled = 0;
+
+	while (1)
+	{
+	//	can_handler();
+		pwm_handler();
+	}
+}
+
