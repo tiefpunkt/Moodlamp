e8f558637f4aeb6ab6d991721087b3c4a2ba1278
Severin Schols <severin@schols.de>
2010-10-26 21:29:24 +0200

    Small optimizations regarding codesize.

--- main.c
+++ main.c
@@ -3,46 +3,25 @@
 *
 **/
 
-
 //#include <avr/io.h>
 #include <avr/interrupt.h>
 
 #include "config.h"
-
-//#include "can/can.h"
-//#include "can_handler.h"
-//#include "can/spi.h"
-//#include "can/lap.h"
-
 #include "pwm.h"
+#include "rc5.h"
+#include "control.h"
 //#include "static_scripts.h"
 //#include "testscript.h"
 
-#include "rc5.h"
-#include "control.h"
-
-void init(void)
-{
-//	ACSR = _BV(ACD); // Disable Analog Comparator (power save)
-
-	//initialize spi port
-//	spi_init();
-	
-	//initialize can communication
-	//can_init();
-	//read_can_addr();
-	//turn on interrupts
-	init_pwm();
-	rc5_init();
-	control_init();
-	sei();
-}
 
 
 int main(void)
 {
-	init();
-	
+//	ACSR = _BV(ACD); // Disable Analog Comparator (power save)
+	init_pwm();
+	rc5_init();
+	control_init();
+	sei();	
 	
 	
 
